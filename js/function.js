af4a123ecdc1013ad070913650477a220c0f4e99
Veasai\User <saikwon88@gmail.com>
2020-09-16 14:07:46 +0700

    function,me
    arrow

---
+++ js/function.js
@@ -0,0 +1,15 @@
+//  function add(a,b) {
+//     return a+b
+// }
+// function mul(a,b){
+//     return a*b
+// }
+// function searhCustomer(customerName){
+//     //find some customer
+
+    const add = (a,b) => {
+        return a + b 
+    }
+
+
+console.log(add(2,3))
