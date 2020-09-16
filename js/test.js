de863f36b64392b19b79233c23f258bd652574d3
Veasai\User <saikwon88@gmail.com>
2020-09-16 13:38:44 +0700

    Create test.js

---
+++ js/test.js
@@ -0,0 +1,16 @@
+let PI = "Sai"
+name = "SAI"
+let test = "Hello"
+name = `${test}S`
+
+let arr = ['apple','banana','kiwi']
+
+console.log("arr :",arr[1])
+
+let person = {
+    name: 'JOKE',
+    age: '15',
+    gender:{ name:'male'},
+    arr
+}
+console.log(person.gender)
