8dc3e83a1093d9cb05655d322aab4041767199f7
Veasai\User <saikwon88@gmail.com>
2020-09-16 15:41:06 +0700

    Create Quiz4.js
    Quiz4

---
+++ js/Quiz4.js
@@ -0,0 +1,12 @@
+ var txt = "Hello my name is joke"
+ var sln = txt.length
+ console.log(sln)
+
+ var str = "Hello my name is joke nice to meet you!"
+ var n = str.search("joke")
+ console.log(n)
+
+ var str ="Well open up your mind and see like me"
+ var res = str.slice(5,10)
+ console.log(res)
+
