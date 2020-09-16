97930d3314808b7175c3e354cfbbce9bc115ea88
Veasai\User <saikwon88@gmail.com>
2020-09-16 15:26:12 +0700

    Create Quiz3.js
    Quiz3

---
+++ js/Quiz3.js
@@ -0,0 +1,14 @@
+score = 70
+
+if (score >=81 && score <=100){
+    grade = "A";
+}else if (score >=71 && score <=80){
+    grade = "B"
+}else if (score >=61 && score <=70){
+    grade = "C"
+}else if (score >=51 && score <=60){
+    grade = "D"
+}else if (score >=0 && score <=50){
+    grade = "F"
+}
+console.log(grade)
