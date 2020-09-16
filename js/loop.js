10de954bf1d45a1df24c5b06e0df60d4a999b49b
Veasai\User <saikwon88@gmail.com>
2020-09-16 14:40:14 +0700

    Create loop.js
    creatloop

---
+++ js/loop.js
@@ -0,0 +1,13 @@
+// for(let i = 1; 1<= 20 ; i+=1){
+//     console.log(i)
+// }
+
+let temp = ['apple','banana','kiwi']
+// temp.forEach((element, idx) =>{
+//     console.log(idx, element)
+// });
+
+let newValue = temp.map((element, idx) => 
+{return`${idx+1}.${element}`})
+console.log(newValue)
+
