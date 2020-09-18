64506f0999261e4f6e00e2be4ad1c3743e972af1
Veasai\User <saikwon88@gmail.com>
2020-09-18 14:10:38 +0700

    up

--- my-resume/App.js
+++ my-resume/App.js
@@ -3,6 +3,8 @@ import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import ResumeFromScreen from './Screen/ResumeForm'
+import ResumeDetailScreen from './Screen/ResumeDetail'
+
 const Stack = createStackNavigator();
 
 function App() {
@@ -10,6 +12,7 @@ function App() {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="ResumeFrom" options={{title:'Resume From'}} component={ResumeFromScreen} />
+        <Stack.Screen name="ResumeDetail" options={{title:'Resume Detail'}} component={ResumeDetailScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
